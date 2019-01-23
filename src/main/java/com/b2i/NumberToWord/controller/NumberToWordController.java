2abb5186d9688f59cee563bb2be9c040b1f9d52c
Saad Benabdallah <benabdallahsaad@hotmail.com>
2019-01-23 14:31:46 -0500

    second commit

--- src/main/java/com/b2i/NumberToWord/controller/NumberToWordController.java
+++ src/main/java/com/b2i/NumberToWord/controller/NumberToWordController.java
@@ -2,7 +2,7 @@ package com.b2i.NumberToWord.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.b2i.NumberToWord.model.Word;
@@ -15,8 +15,8 @@ public class NumberToWordController {
 	@Autowired
 	private NumberToWord numberToWord;
 	
-	@GetMapping(value = "number/{value}")
-	public Word convertController(@PathVariable Long value) {
+	@GetMapping(value = "writeNumber")
+	public Word convertController(@RequestParam("value") Long value) {
 		Word word = new Word();
 		word.setContent(numberToWord.convert(value));
 		return word;
